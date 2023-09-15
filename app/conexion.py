24a0369cfe4ed79593804a6afcada404f9e0b470
Sergio Esteban Alfonso <andresmt1119@gmail.com>
2023-09-15 16:23:54 -0500

    Agrega base de datos, crea clase base de datos con los metodos necesarios para insertar, eliminar, marcar como hecha y leer las tareas

---
+++ app/conexion.py
@@ -0,0 +1,46 @@
+# trae variables de entorno
+import os
+from pyodbc import connect
+
+class BaseDeDatos:
+    def __init__(self) -> None:
+        self.conexion = connect(r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\cuc\buenas_practicas2\tasks;")
+        self.cursor = self.conexion.cursor()
+
+    def insertar_tarea(self, tarea):
+        query = "INSERT INTO tareas (Descripcion, Estado) \
+        VALUES (?, No)"
+        self.cursor.execute(query, (tarea))
+        self.conexion.commit()
+
+    def completar_tarea(self, id_tarea):
+        query = " UPDATE tareas SET Estado = Si WHERE Id = ?;"
+        self.cursor.execute(query, id_tarea)
+        self.conexion.commit()
+    
+    def eliminar_tarea(self, id_tarea):
+        query = "DELETE FROM Empleados WHERE ID = ?"
+        self.cursor.execute(query, id_tarea)
+        self.conexion.commit()
+
+    def leer_tareas(self):
+        self.cursor.execute("SELECT * FROM tareas")
+        return self.cursor.fetchall()
+
+
+if __name__ == '__main__':
+    # se instancia la base de datos para poder usarla en el proyecto
+    db = BaseDeDatos()
+
+    # se llena la base de datos
+    tareas = [
+        'Llenar la jarra',
+        'Hacer deploy',
+        'Organizar el cuarto'
+        'barrer el cuarto',
+        'Estudiar algoritmos 2',
+        'Lavar ropa',
+        'Revisar material de SED'
+    ]
+    for tarea in tareas:
+        db.insertar_tarea(tarea)
