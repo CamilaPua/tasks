a9a62324ab11ea6ce86e10f873ecd0c64b8285be
Camila <camilapua7@gmail.com>
2023-09-22 08:21:21 -0500

    Agrega funcionalidades para eliminar y cambiar estado de una tarea

--- app/forms.py
+++ app/forms.py
@@ -5,4 +5,6 @@ class Tarea(FlaskForm):
     id = IntegerField('ID de la tarea')
     descripcion = StringField('Descripcion de la tarea')
     hecha = SubmitField('Hecha')
+    no_hecha = SubmitField('No hecha')
+    eliminar = SubmitField('Eliminar')
     submit = SubmitField('Enviar')
--- app/main.py
+++ app/main.py
@@ -26,10 +26,20 @@ def agregar_tarea():
 
 @app.route('/modificar/', methods=['GET', 'POST'])
 def modificar_tarea():
+    lista_dics_tareas = db.leer_tareas()
     tareas_form = forms.Tarea(request.form)
     if request.method == 'POST':
-        pass
-    return render_template('modificar_tarea.html', tareas_form=tareas_form)
+        id_tarea = tareas_form.id.data
+        if id_tarea:
+            eliminar_tarea = tareas_form.eliminar.data
+            tarea_hecha, tarea_no_hecha = tareas_form.hecha.data, tareas_form.no_hecha.data
+            if eliminar_tarea:
+                db.eliminar_tarea(id_tarea)
+            elif tarea_hecha:
+                db.cambiar_estado_tarea(id_tarea, True)
+            elif tarea_no_hecha:
+                db.cambiar_estado_tarea(id_tarea, False)
+    return render_template('modificar_tarea.html', tareas_form=tareas_form, lista_dics_tareas=lista_dics_tareas)
 
 
 if __name__ == '__main__':
